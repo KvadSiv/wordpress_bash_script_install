fd5f5ed5a66e81c9d7d2b68e4ad452d6a2671a70
KvadSiv <61190704+KvadSiv@users.noreply.github.com>
2022-07-18 18:01:41 +0600

    update

--- 1_upd_inst.sh
+++ 1_upd_inst.sh
@@ -9,13 +9,13 @@ echo -en '\033[1;33mВыберите ваш язык использования/
 				sudo apt autoremove -y && apt autoclean -y
 		echo -en '\033[1;33mСистема обновлена \033[0m \n'
 		echo -en '\033[1;33mНачинается установка Apache2..\033[0m \n'
-				sudo apt install apache2 apache2-utils
+				sudo apt install apache2 apache2-utils -y
 				sudo systemctl enable apache2
 				sudo systemctl start apache2
 		echo -en '\033[1;33mНачинается установка Mysql.. \033[0m \n'
-				sudo apt install mysql-client mysql-server
+				sudo apt install mysql-client mysql-server -y
 		echo -en '\033[1;33mНачинается установка Php7.4.. \033[0m \n'
-				sudo apt install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
+				sudo apt install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd php7.4-curl php7.4-dom php7.4-imagick php7.4-mbstring php7.4-zip php7.4-intl -y
 				sudo a2enmod rewrite
 				sudo systemctl restart apache2
 		echo -en '\033[1;33mНачинается скачивание Wordpress.. \033[0m \n'
@@ -36,13 +36,13 @@ echo -en '\033[1;33mВыберите ваш язык использования/
 				sudo apt autoremove -y && apt autoclean -y
 		echo -en '\033[1;33mSystem updated \033[0m \n'
 		echo -en '\033[1;33mInstallation of Apache2 begins..\033[0m \n'
-				sudo apt install apache2 apache2-utils
+				sudo apt install apache2 apache2-utils -y
 				sudo systemctl enable apache2
 				sudo systemctl start apache2
 		echo -en '\033[1;33mMysql installation starts.. \033[0m \n'
-				sudo apt install mysql-client mysql-server
+				sudo apt install mysql-client mysql-server -y
 		echo -en '\033[1;33mPhp7.4 installation starts.. \033[0m \n'
-				sudo apt install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
+				sudo apt install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd php7.4-curl php7.4-dom php7.4-imagick php7.4-mbstring php7.4-zip php7.4-intl -y
 				sudo a2enmod rewrite
 				sudo systemctl restart apache2
 		echo -en '\033[1;33mWordpress download starts.. \033[0m \n'
--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Wordpress simple install scripts
 ## Quick start
 
 - `git clone https://github.com/KvadSiv/wordpress_bash_script_install.git`
-- `cd wordpress_bash_script_install.git/`
-- `find ./ -name "*.sh" -exec chmod +x {} \;`
+- `cd wordpress_bash_script_install`
+- `chmod +x 1_upd_inst.sh`
+- `chmod +x 2_user_db.sh`
 - `./1_upd_inst.sh`
 - `./2_user_db.sh`
